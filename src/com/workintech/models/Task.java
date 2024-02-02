e8137abedb8532acc138430f0f075242e8cb476d
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-02 15:02:11 +0300

    constructor olusturuldu

--- src/com/workintech/models/Task.java
+++ src/com/workintech/models/Task.java
@@ -9,4 +9,12 @@ public class Task {
     private String assignee;
     private Priority priority;
     private Status status;
+
+    public Task(String project, String description, String assignee, Priority priority, Status status) {
+        this.project = project;
+        this.description = description;
+        this.assignee = assignee;
+        this.priority = priority;
+        this.status = status;
+    }
 }
